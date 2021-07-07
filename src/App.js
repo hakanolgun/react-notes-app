619444295bd09be31364582fd7b5b62b7b5ff1ff
hakanolgun <kaptanhakanolgun@gmail.com>
2021-07-07 10:39:24 +0300

    bootstrap and css imporements, useState for date and time

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { useState, useEffect } from "react";
+import { Button } from "react-bootstrap";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const date = new Date().toLocaleDateString();
+  const dateN = new Date().toLocaleDateString();
+  const timeN = new Date().toLocaleTimeString();
+
+  const [date, setDate] = useState(dateN);
+  const [time, setTime] = useState(timeN);
+
+  // setTime({
+  //   time = new Date().toLocaleTimeString()
+  // });
 
   //Websayfası ilk açıldığında, Not yazma kısmı, eğer local storagede varsa gösterilsin yoksa gösterilmesin
   const [showState, setShowState] = useState(() => {
@@ -16,7 +26,6 @@ function App() {
   //Notun içindeki yazıları varsa local storage'den çek yoksa boş bırak
   const [notetext, setNoteText] = useState("");
 
-  
   //yeni not ekleme fonksiyonu
   const addNewElement = () => {
     setShowState(true);
@@ -26,6 +35,9 @@ function App() {
     myDiv[0].classList.remove("noshowcss");
 
     localStorage.setItem("myNotes", notetext);
+
+    setDate(new Date().toLocaleDateString());
+    setTime(new Date().toLocaleTimeString())
   };
 
   const deleteNote = () => {
@@ -35,38 +47,45 @@ function App() {
     localStorage.clear();
     let myTextarea = document.getElementById("notearea");
     myTextarea.value = "";
-    
-  }
-
+    setDate(new Date().toLocaleDateString());
+    setTime(new Date().toLocaleTimeString())
+  };
 
   //textarea'ya bir şeyler yazıldığında bunu yakala
   const handleInput = (e) => {
     setNoteText(e.target.value);
     localStorage.setItem("myNotes", e.target.value);
+    setDate(new Date().toLocaleDateString());
+    setTime(new Date().toLocaleTimeString())
   };
 
   useEffect(() => {
-    if(localStorage.myNotes && localStorage.myNotes !== ""){
-      setNoteText(localStorage.myNotes)
+    if (localStorage.myNotes && localStorage.myNotes !== "") {
+      setNoteText(localStorage.myNotes);
     }
-  }, [])
+  }, []);
 
   return (
     <div className="App">
-      <p>
-        React Notes App - Hakan{" "}
-        <button onClick={() => addNewElement()}>Not Ekle</button>
-      </p>
+      <div className="titleP">
+        <h1>React Notes App</h1>
+        <Button className="btn btn-primary" onClick={() => addNewElement()}>
+          Not Ekle
+        </Button>
+      </div>
       <div
         className={`kapsayiciNotDivi ${
           !showState ? "noshowcss" : "kapsayiciNotDivi"
         }`}
       >
         <div className="tarihparagrafi">
-          <p>{date}</p>
           <div>
-            <button>Edit</button>
-            <button onClick={()=>deleteNote()}>Delete</button>
+            <p>{date} {time}</p>
+          </div>
+          <div>
+            <Button className="btn btn-danger" onClick={() => deleteNote()}>
+              Delete
+            </Button>
           </div>
         </div>
         <textarea
